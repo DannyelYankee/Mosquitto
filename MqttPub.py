aa3e14a72e01da03352f145e15ebbb810a587c1f
DannyelYankee <daniel.juape@gmail.com>
2021-12-10 20:51:59 +0100

    comentarios añadidos

--- MqttPub.py
+++ MqttPub.py
@@ -1,19 +1,6 @@
 import paho.mqtt.client as mqtt
-
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    
-
-
-    client.subscribe("test/#")
-
-
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-
+from datetime import datetime
 client = mqtt.Client()
-client.on_connect= on_connect
-client.on_message= on_message
-
 client.connect("mqtt", 1883, 60)
-client.loop_forever()
+name = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+client.publish("test/"+name,  "hola")
--- MqttSub.py
+++ MqttSub.py
@@ -1,6 +1,19 @@
 import paho.mqtt.client as mqtt
-from datetime import datetime
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    
+
+
+    client.subscribe("test/#")
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
 client = mqtt.Client()
+client.on_connect= on_connect
+client.on_message= on_message
+
 client.connect("mqtt", 1883, 60)
-name = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-client.publish("test/"+name,  "hola")
+client.loop_forever()
